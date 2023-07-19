dfe5a034da51e17951376252f3b960e090648abf
yangofar <2423250587@qq.com>
2023-07-19 11:42:51 +0800

    第一次更新

---
+++ src/main/java/com/example/gitstudylocal/GitstudyLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitstudylocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitstudyLocalApplication.class, args);
+	}
+
+}
